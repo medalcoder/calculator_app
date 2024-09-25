5fa8aa62d074755fcf22756e9ced4a959d16f7ea
AbdulHameed Abidemi <hammedadeosun@gmail.com>
2024-09-25 23:58:58 +0100

    app updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,21 @@
+//function to display an input 
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
 
+//function to clear all input at once
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
+//function to clear an input at a go
+/*
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+*/
+
+//function that calculate input along with all operators
 function calculate() {
     try {
         document.getElementById('display').value = eval(document.getElementById('display').value);
